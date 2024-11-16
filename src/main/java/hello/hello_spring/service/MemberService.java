a63777cd4da31d1894221c27eed797cb99195554
soob <dlvtoekf4@naver.com>
2024-11-17 05:09:01 +0900

    section 5 - component scan

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 회원 서비스 코드를 DI 가능하게 변경
     private final MemberRepository memberRepository;
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
