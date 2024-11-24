c4a90b3472aa3ea6ad06c33dfa45f79552e0e8dc
soob <dlvtoekf4@naver.com>
2024-11-24 18:27:54 +0900

    section 7 - JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,10 +1,8 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JdbcMemberRepository;
-import hello.hello_spring.repository.JdbcTemplateMemberRepository;
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,9 +12,15 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
     private final DataSource dataSource;  // DB 커네션을 획득하기 위해 사용
+    private final EntityManager em;
 
-    public SpringConfig(DataSource dataSource) {
+    /*public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }*/
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -28,6 +32,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
