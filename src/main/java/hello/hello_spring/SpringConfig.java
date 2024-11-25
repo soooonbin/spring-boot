401dfe2865ecb907703adc195ca645aeccdb0dc7
soob <dlvtoekf4@naver.com>
2024-11-25 12:16:14 +0900

    section 7 - 스프링 데이터 JPA

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -11,28 +11,15 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;  // DB 커네션을 획득하기 위해 사용
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    /*public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }*/
-
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
 }
